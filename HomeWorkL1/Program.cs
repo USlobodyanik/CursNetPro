0abb9906227a0569c05f8c845d2491f91abcd062
Yuriy Slobodyanik <uslobodyanik@gmail.com>
2021-11-01 17:54:05 +0200

    Home work, Task2

--- HomeWorkL1/Program.cs
+++ HomeWorkL1/Program.cs
@@ -4,7 +4,6 @@ namespace HomeWorkL1
 {
     class Program
     {
-
         static void Task1()
         {
             double a;
@@ -23,10 +22,28 @@ namespace HomeWorkL1
             Console.WriteLine($"Result for task1:{Environment.NewLine}{result}");
 
         }
+        static void Task2()
+        {
+            string a;
+            string b;
+
+            Console.WriteLine("Enter string 1: ");
+            a = Console.ReadLine();
+            string temp = a;
+
+            Console.WriteLine("Enter string 2: ");
+            b = Console.ReadLine();
+            
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"Result String 1: " + a + " String 2: " + b);
+
+        }
         static void Main(string[] args)
         {
-            Task1();
-            Console.ReadKey();
+
+            Task2();
 
         }
     }
