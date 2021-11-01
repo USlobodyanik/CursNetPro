78614c230dba04d8bc7f65b022930bb61a17db6d
Yuriy Slobodyanik <uslobodyanik@gmail.com>
2021-11-01 15:41:19 +0200

    Homework Lesson1, Task1

--- HomeWorkL1/Program.cs
+++ HomeWorkL1/Program.cs
@@ -4,9 +4,30 @@ namespace HomeWorkL1
 {
     class Program
     {
+
+        static void Task1()
+        {
+            double a;
+            double b;
+
+            Console.Write("Enter numder A: ");
+            string input = Console.ReadLine();
+            a = Convert.ToDouble(input);
+
+            Console.Write("Enter numder B: ");
+            input = Console.ReadLine();
+            b = Convert.ToDouble(input);
+
+            double result = (5 * a + b * b) / (b - a);
+
+            Console.WriteLine($"Result for task1:{Environment.NewLine}{result}");
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task1();
+            Console.ReadKey();
+
         }
     }
 }
