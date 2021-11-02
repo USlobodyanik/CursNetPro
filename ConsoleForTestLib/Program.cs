e895bfe30e25b60fdaa9abc7c1f61c73ce1a9155
Yuriy Slobodyanik <uslobodyanik@gmail.com>
2021-11-02 21:56:34 +0200

    Home work L1, Task2 work with lib

--- ConsoleForTestLib/Program.cs
+++ ConsoleForTestLib/Program.cs
@@ -9,8 +9,14 @@ namespace ConsoleForTestLib
         {
 
             Console.WriteLine("Task1 work with lib: ");
-
             Console.WriteLine(MyTestLibrary.Task1(4.5, 7));
+
+            Console.WriteLine("Task2 work with lib: ");
+            string a = "String1";
+            string b = "String2";
+            Console.WriteLine($"Input str: {a} {b}");
+            MyTestLibrary.Task2(ref a, ref b);
+            Console.WriteLine($"Replaced str: {a} {b}");
 
         }
     }
